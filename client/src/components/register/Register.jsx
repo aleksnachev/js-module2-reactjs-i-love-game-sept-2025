6c904885d2906bfe7aa1e88cb0032296572d9f7c
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-29 20:36:11 +0200

    fix some things

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,14 +1,28 @@
 export default function Register() {
+    const registerSubmit = (formData) => {
+        const email = formData.get('email')
+        const password = formData.get('password')
+        const confirmPassword = formData.get('confirm-password')
+
+        //TODO: Validation
+        
+
+    }
+
     return (
         <>
-            {/* Register Page ( Only for Guest users ) */}
             <section id="register-page" className="content auth">
-                <form id="register">
+                <form id="register" action={registerSubmit()}>
                     <div className="container">
                         <div className="brand-logo" />
                         <h1>Register</h1>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Your Email" />
+                        <input 
+                            type="email" 
+                            id="email" 
+                            name="email" 
+                            placeholder="Your Email"
+                        />
                         <label htmlFor="pass">Password:</label>
                         <input
                             type="password"

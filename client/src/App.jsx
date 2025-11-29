388f7a1436589ed1fb7546fc844673ada795ebc6
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-29 20:30:10 +0200

    add header links and register basic form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home.jsx"
 import Catalog from "./components/catalog/Catalog.jsx"
 import Details from "./components/details/Details.jsx"
 import GameCreate from "./components/game-create/GameCreate.jsx"
+import Register from "./components/register/Register.jsx"
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route path = "/games" element = {<Catalog/>}/>
                     <Route path = "/games/:gameId/details" element = {<Details/>}/>
                     <Route path = "/games/create" element = {<GameCreate/>}/>
+                    <Route path = "/register" element = {<Register/>}/>
 
 
                 </Routes>

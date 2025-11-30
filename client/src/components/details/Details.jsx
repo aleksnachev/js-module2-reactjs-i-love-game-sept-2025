3debddfb3b9baec65a7f2f7e4ebb20dd55638992
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-30 15:45:49 +0200

    submit comment functionality

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { Link, useNavigate, useParams } from "react-router"
 import CreateComment from "./create-comment/CreateComment.jsx"
+import DetailsComments from "./details-comments/DetailsComments.jsx"
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
-export default function Details() {
+export default function Details({
+    user
+}) {
     const navigate= useNavigate()
     const {gameId} = useParams()
     const [game,setGame] = useState({})
@@ -71,28 +74,11 @@ export default function Details() {
                     {/* <Link to={`/games/${gameId}/delete`} className="button">Delete</Link> */}
                     <button className="button" onClick={deleteGameHandler}>Delete</button>
                 </div>
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        <li className="comment">
-                            <p>
-                                Content: A masterpiece of world design, though the boss fights are
-                                brutal.
-                            </p>
-                        </li>
-                        <li className="comment">
-                            <p>
-                                Content: Truly feels like a next-gen evolution of the Souls formula!
-                            </p>
-                        </li>
-                    </ul>
-                    {/* Display paragraph: If there are no games in the database */}
-                    {/* <p class="no-comment">No comments.</p> */}
-                </div>
-            </div>
+                <DetailsComments/>
+            </div >
             {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
 
-            <CreateComment/>
+            <CreateComment user={user}/>
         </section>
 
     )
--- client/src/components/details/create-comment/CreateComment.jsx
+++ client/src/components/details/create-comment/CreateComment.jsx
@@ -1,10 +1,35 @@
-export default function CreateComment() {
+import { useState } from "react"
+import request from "../../../utils/request.js"
+import { useParams } from "react-router"
+
+export default function CreateComment({
+    user
+}) {
+    const {gameId} = useParams()
+    const [comment,setComment] = useState('')
+    const changeHandler = (e) => {
+        setComment(e.target.value)
+    }
+
+    const submitHandler = async () => {
+        await request('/comments', 'POST', {
+            author: user.email,
+            message: comment,
+            gameId
+        })
+    }
     return (
         <article className="create-comment">
             <label>Add new comment:</label>
-            <form className="form">
-                <textarea name="comment" placeholder="Comment......" defaultValue={""} />
-                <input className="btn submit" type="submit" defaultValue="Add Comment" />
+            <form className="form" action={submitHandler}>
+                <textarea 
+                name="comment" 
+                placeholder="Comment......"
+                onChange={changeHandler}
+                value={comment}
+                defaultValue={""}
+                />
+                <input className="btn submit" type="submit" defaultValue="Add Comment" disabled={!user}/>
             </form>
         </article>
     )
